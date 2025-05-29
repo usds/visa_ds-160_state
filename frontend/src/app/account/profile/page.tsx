9868f6212a7ca9fd2d6fdc46903c8273358ec69b
Kristine <167804653+KristineSarnlertsophonUSDS@users.noreply.github.com>
2025-05-29 03:04:42 +0000

    remove unnecessary querying in profile page

--- frontend/src/app/account/profile/page.tsx
+++ frontend/src/app/account/profile/page.tsx
@@ -1,26 +1,28 @@
+"use client";
+
 import React from "react";
 import "@trussworks/react-uswds/lib/uswds.css";
 import "@trussworks/react-uswds/lib/index.css";
-import {
-  HydrationBoundary,
-  QueryClient,
-  dehydrate,
-} from "@tanstack/react-query";
-
 import "@/app/globals.css";
-import { getUserFromSession } from "@/api/session";
-import Profile from "./profile";
 
-export default async function ProfilePage() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["sessionuser"],
-    queryFn: getUserFromSession,
-  });
+import { useUser } from "@/providers/UserContext";
+
+export default function ProfilePage() {
+  const { user, isLoading, error } = useUser();
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Profile />
-    </HydrationBoundary>
+    <div>
+      <h1>Profile</h1>
+      <p>Welcome to the profile page!</p>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : user ? (
+        <p>Logged in as: {user.email}</p>
+      ) : (
+        <p>You are not logged in.</p>
+      )}
+    </div>
   );
-}
+};
