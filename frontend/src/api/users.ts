63b5bbb3178f9c4bea04ec6daa7aeaaf36faa813
Kristine <167804653+KristineSarnlertsophonUSDS@users.noreply.github.com>
2025-05-29 02:37:18 +0000

    add types back

--- frontend/src/api/users.ts
+++ frontend/src/api/users.ts
@@ -1,12 +1,13 @@
+import { User } from "@/types";
 import { fetchJson, baseURL } from "./utils";
 
-export const getUsers = () =>
+export const getUsers = (): Promise<User[]> =>
   fetchJson(`${baseURL}/users/`, { cache: "no-store" });
 
-export const getUserByEmail = (email: string) =>
+export const getUserByEmail = (email: string): Promise<User> =>
   fetchJson(`${baseURL}/users/${email}/`, { cache: "no-store" });
 
-export const createUser = (user: { email: string }) =>
+export const createUser = (user: { email: string }): Promise<User> =>
   fetchJson(`${baseURL}/users/`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
